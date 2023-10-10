2d32b48b9fb953f09e2d969fe74f204dbbbb27ad
Captain <243313024@qq.com>
2023-10-10 16:14:28 +0800

    can测试

--- Core/Src/stm32f4xx_it.c
+++ Core/Src/stm32f4xx_it.c
@@ -252,6 +252,20 @@ void DMA1_Stream4_IRQHandler(void)
 }
 
 /**
+  * @brief This function handles CAN1 TX interrupts.
+  */
+void CAN1_TX_IRQHandler(void)
+{
+  /* USER CODE BEGIN CAN1_TX_IRQn 0 */
+
+  /* USER CODE END CAN1_TX_IRQn 0 */
+  HAL_CAN_IRQHandler(&hcan1);
+  /* USER CODE BEGIN CAN1_TX_IRQn 1 */
+
+  /* USER CODE END CAN1_TX_IRQn 1 */
+}
+
+/**
   * @brief This function handles CAN1 RX0 interrupts.
   */
 void CAN1_RX0_IRQHandler(void)
@@ -335,7 +349,6 @@ void CAN2_RX1_IRQHandler(void)
   /* USER CODE END CAN2_RX1_IRQn 0 */
   HAL_CAN_IRQHandler(&hcan2);
   /* USER CODE BEGIN CAN2_RX1_IRQn 1 */
- 
 
   /* USER CODE END CAN2_RX1_IRQn 1 */
 }
--- Core/Usr/can.c
+++ Core/Usr/can.c
@@ -9,7 +9,20 @@
 ******************************************************************************************************************/
 
 #include "can.h"
-void CAN_Config(CAN_HandleTypeDef *hcan,CAN_TxHeaderTypeDef *TxHeader)
+#include "main.h"
+
+extern CAN_HandleTypeDef hcan1;
+extern CAN_HandleTypeDef hcan2;
+extern CAN_TxHeaderTypeDef	CAN1TxHeader;      //CAN1发送结构体
+extern CAN_TxHeaderTypeDef	CAN2TxHeader;      //CAN2发送结构体
+extern CAN_RxHeaderTypeDef	CAN1RxHeader;      //CAN1接收结构体
+extern CAN_RxHeaderTypeDef	CAN2RxHeader;      //CAN2接收结构体
+extern u8	CAN1TxData[8];    //CAN1发送的数据
+extern u8	CAN2TxData[8];    //CAN2发送的数据
+extern u8	CAN1RxData[8];    //CAN1接收的数据
+extern u8	CAN2RxData[8];    //CAN2接收的数据
+
+void CAN_Config(CAN_HandleTypeDef *hcan,CAN_TxHeaderTypeDef *TxHeader, u32 Rec_Interrupts)
 {
   CAN_FilterTypeDef  sFilterConfig;
 
@@ -43,7 +56,7 @@ void CAN_Config(CAN_HandleTypeDef *hcan,CAN_TxHeaderTypeDef *TxHeader)
   }
 
   /*##-4- Activate CAN RX notification #######################################*/
-  if (HAL_CAN_ActivateNotification(hcan, CAN_IT_RX_FIFO0_MSG_PENDING) != HAL_OK)
+  if (HAL_CAN_ActivateNotification(hcan, Rec_Interrupts) != HAL_OK)
   {
     /* Notification Error */
     while(1)
@@ -86,3 +99,16 @@ u8 CAN_Send_Msg(CAN_HandleTypeDef *hcan,CAN_TxHeaderTypeDef *TxHeader,u8* msg,u8
 	while(HAL_CAN_GetTxMailboxesFreeLevel(hcan) != 3) {}
     return 0;
 }
+
+void HAL_CAN_RxFifo1MsgPendingCallback(CAN_HandleTypeDef *hcan)
+{
+	  // CAN数据接收
+	  if (hcan->Instance == hcan2.Instance)
+	  {
+		  if (HAL_CAN_GetRxMessage( hcan, CAN_RX_FIFO0, &CAN2RxHeader, CAN2RxData) == HAL_OK)
+		  {
+			 HAL_CAN_ActivateNotification(hcan, CAN_IT_RX_FIFO0_MSG_PENDING);						  // 再次使能FIFO0接收中断
+		  }
+	  }
+}
+
